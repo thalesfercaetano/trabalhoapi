a17939a48849309900699196200ac954df379c9e
Thales <thalesfercaetano@gmail.com>
2025-09-21 16:38:57 -0300

    Feito

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ type users = {
 let users: users[] = [
 
     { id: 1, name: "Flávio", email: "flavio@flavio.com", senha: "flavio", age: 25, role: "admin" },
-    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123", age: 22, role: "user" },
+    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123", age: 20, role: "user" },
     { id: 3, name: "Mariana", email: "mariana@teste.com", senha: "mari2025", age: 28, role: "user" },
     { id: 4, name: "João", email: "joao@teste.com", senha: "joao321", age: 30, role: "user" },
     { id: 5, name: "Carla", email: "carla@teste.com", senha: "carla456", age: 27, role: "user" },
