112e446bfd827339228572513781c57a3a1590be
Thales <thalesfercaetano@gmail.com>
2025-09-20 23:36:16 -0300

    oiie flaviooo

--- src/bd.ts
+++ src/bd.ts
@@ -1,10 +1,24 @@
 export const users = [
-    
-    { id: 1, name: "Flávio", email: "flavio@flavio.com", senha: "flavio" },
-    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123" },
-    { id: 3, name: "Mariana", email: "mariana@teste.com", senha: "mari2025" },
-    { id: 4, name: "João", email: "joao@teste.com", senha: "joao321" },
-    { id: 5, name: "Carla", email: "carla@teste.com", senha: "carla456" },
-    { id: 6, name: "Lucas", email: "lucas@teste.com", senha: "lucas789" }
-
+    { id: 1, name: "Flávio", email: "flavio@flavio.com", senha: "flavio", age: 25 },
+    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123", age: 22 },
+    { id: 3, name: "Mariana", email: "mariana@teste.com", senha: "mari2025", age: 28 },
+    { id: 4, name: "João", email: "joao@teste.com", senha: "joao321", age: 30 },
+    { id: 5, name: "Carla", email: "carla@teste.com", senha: "carla456", age: 27 },
+    { id: 6, name: "Lucas", email: "lucas@teste.com", senha: "lucas789", age: 24 }
 ];
+
+
+export type post = {    
+    id: number,
+    title: string,
+    content: string,
+    authorId: number,
+    createdAt: Date,
+    published: boolean,
+
+};
+
+
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import cors from "cors";
 import express from "express";
 
-import { users } from "./bd";
-
 const app = express();
 
 app.use(cors());
@@ -11,6 +9,48 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Tafunfando");
+});
+
+type users = {
+    id: number;
+    name: string;
+    email: string;
+    senha: string;
+    age: number;
+}
+
+const users: users[] = [
+
+    { id: 1, name: "Flávio", email: "flavio@flavio.com", senha: "flavio", age: 25 },
+    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123", age: 22 },
+    { id: 3, name: "Mariana", email: "mariana@teste.com", senha: "mari2025", age: 28 },
+    { id: 4, name: "João", email: "joao@teste.com", senha: "joao321", age: 30 },
+    { id: 5, name: "Carla", email: "carla@teste.com", senha: "carla456", age: 27 },
+    { id: 6, name: "Lucas", email: "lucas@teste.com", senha: "lucas789", age: 24 }
+
+];
+
+type post  = {    
+    id: number,
+    title: string,
+    content: string,
+    authorId: number,
+    createdAt: Date,
+    published: boolean,
+
+};
+
+app.get("/users/age-range", (req, res) => {
+    const minAge = parseInt(req.query.min as string);
+    const maxAge = parseInt(req.query.max as string);
+
+    if (isNaN(minAge) || isNaN(maxAge)) {
+        return res.status(400).send("Parâmetros inválidos");
+    }
+    const filteredUsers = users.filter(user => user.age >= minAge && user.age <= maxAge);
+
+    return res.json(filteredUsers);
+
 });
 
 app.get("/users/:id", (req, res) => {
@@ -22,7 +62,13 @@ app.get("/users/:id", (req, res) => {
         return res.status(404).send("Usuário não encontrado");
     }
 
+    return res.json(user);
+
+
 });
+
+
+
 
 
 app.listen(3003, () => {
