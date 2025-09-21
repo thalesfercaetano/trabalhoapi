81890d07c37cc98ed8209c67afe494a48513f8cc
Thales <thalesfercaetano@gmail.com>
2025-09-20 23:08:20 -0300

    com bd

---
+++ src/bd.ts
@@ -0,0 +1,10 @@
+export const users = [
+    
+    { id: 1, name: "Flávio", email: "flavio@flavio.com", senha: "flavio" },
+    { id: 2, name: "Thales", email: "thales@teste.com", senha: "thales123" },
+    { id: 3, name: "Mariana", email: "mariana@teste.com", senha: "mari2025" },
+    { id: 4, name: "João", email: "joao@teste.com", senha: "joao321" },
+    { id: 5, name: "Carla", email: "carla@teste.com", senha: "carla456" },
+    { id: 6, name: "Lucas", email: "lucas@teste.com", senha: "lucas789" }
+
+];
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
 import cors from "cors";
 import express from "express";
 
+import { users } from "./bd";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Tafunfando");
 });
+
+app.get("/users/:id", (req, res) => {
+
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).send("Usuário não encontrado");
+    }
+
+});
+
 
 app.listen(3003, () => {
   console.log("Servidor rodando na porta 3003");
