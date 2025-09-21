e0e56271caa44c4d76a555594e2b31e99c67cab7
Thales <thalesfercaetano@gmail.com>
2025-09-20 22:46:04 -0300

    atualizadin

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Server is starting...");
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3003, () => {
+  console.log("Servidor rodando na porta 3003");
+});
